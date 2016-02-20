d6d7154f8421085a7f9e1dda4b65c42cfbd5f2de
Sergey Kuznetsov <votkinsk@gmail.com>
2016-02-20 22:01:51 +0300

    Create SnapshotMode.user.js

---
+++ SnapshotMode.user.js
@@ -0,0 +1,13 @@
+// ==UserScript==
+// @name         Enable Snapshot Mode
+// @namespace    https://BraydenPeacock.ca/waze/scripts
+// @version      0.3
+// @description  Enables the use of WME Snapshot mode (top right corner near country select)
+// @author       CoolCanuck
+// @updateURL    https://BraydenPeacock.ca/waze/scripts/SnapshotMode.user.js
+// @include    	 /^https://(editor-beta|www).waze.com/(.*/)?editor/.*/
+// @exclude      https://www.waze.com/user/*
+// @grant        GM_addStyle
+// ==/UserScript==
+
+GM_addStyle("#snapshot-toggler { display: inline !important; }");
