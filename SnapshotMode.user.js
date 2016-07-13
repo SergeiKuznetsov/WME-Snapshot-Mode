cebb3d20c9719d1a88cf0a60bbe131703b7681e9
Sergey Kuznetsov <votkinsk@gmail.com>
2016-07-13 12:21:27 -0700

    new beta domain

--- SnapshotMode.user.js
+++ SnapshotMode.user.js
@@ -5,7 +5,7 @@
 // @description  Enables the use of WME Snapshot mode (top right corner near country select)
 // @author       CoolCanuck
 // @updateURL    https://BraydenPeacock.ca/waze/scripts/SnapshotMode.user.js
-// @include    	 /^https://(editor-beta|www).waze.com/(.*/)?editor/.*/
+// @include    	 /^https://(editor-beta|www|beta).waze.com/(.*/)?editor/.*/
 // @exclude      https://www.waze.com/user/*
 // @grant        GM_addStyle
 // ==/UserScript==
